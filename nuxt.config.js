f44dc998965f587d95889a1bdde7fb9283256003
Alexander Lichter <github@lichter.io>
2021-02-07 01:13:23 +0100

    init

---
+++ nuxt.config.js
@@ -0,0 +1,5 @@
+export default {
+  target: 'static',
+  buildModules:  ['@nuxtjs/composition-api'],
+  components: true
+}
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Test</div>
+</template>
+
+<script>
+import { useFetch, ref } from '@nuxtjs/composition-api'
+
+export default {
+  setup () {
+    useFetch(() => {})
+  }
+}
+</script>
